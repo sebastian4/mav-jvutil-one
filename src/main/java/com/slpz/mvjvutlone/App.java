a14878eedabc393dcdec5e93dbb81c18cd26d89a
sebastian <sebastian.lopezx@yahoo.com>
2014-03-10 23:04:34 -0400

    updated app testing the trackdto

--- src/main/java/com/slpz/mvjvutlone/App.java
+++ src/main/java/com/slpz/mvjvutlone/App.java
@@ -1,9 +1,56 @@
 package com.slpz.mvjvutlone;
 
+import java.util.List;
+
+import com.slpz.mvjvutlone.dto.TrackDto;
+import com.slpz.mvjvutlone.model.Track;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hi hello hello hello" );
+        tst1();
+    }
+    
+    public static void tst1() {
+    	
+    	TrackDto trackDto = new TrackDto();
+    	
+    	List<Track> tracks1 = trackDto.getAllTracks();
+    	for (Track track : tracks1) {
+    		System.out.println(track);
+    	}
+    	System.out.println("------");
+    	
+    	Track track11 = trackDto.getTrackByTitle("One");
+    	System.out.println(track11);
+    	System.out.println("------");
+    	
+    	Track track12 = trackDto.getTrackById(12);
+    	System.out.println(track12);
+    	System.out.println("------");
+    	
+    	Track track22 = new Track(22,"Jude","Beatles");
+    	Track track23 = new Track(23,"Submarine","Beatles");
+    	String result20 = trackDto.createTrack(track22);
+    	System.out.println(result20);
+    	result20 = trackDto.createTrack(track23);
+    	System.out.println(result20);
+    	tracks1 = trackDto.getAllTracks();
+    	for (Track track : tracks1) {
+    		System.out.println(track);
+    	}
+    	System.out.println("------");
+    	
+    	Track track24 = new Track(13,"1999","Pineapples");
+    	result20 = trackDto.updateTrack(track24);
+    	System.out.println(result20);
+    	trackDto.deleteTrackById(12);
+    	tracks1 = trackDto.getAllTracks();
+    	for (Track track : tracks1) {
+    		System.out.println(track);
+    	}
+    	System.out.println("------");
     }
 }
--- src/main/java/com/slpz/mvjvutlone/dto/TrackDto.java
+++ src/main/java/com/slpz/mvjvutlone/dto/TrackDto.java
@@ -1,7 +1,10 @@
 package com.slpz.mvjvutlone.dto;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+
 import com.slpz.mvjvutlone.model.Track;
 
 public class TrackDto {
@@ -10,8 +13,27 @@ public class TrackDto {
 	
 	static {
 		tracks.put(11, new Track(11,"One","U2"));
-		tracks.put(11, new Track(12,"Loosing","REM"));
-		tracks.put(11, new Track(13,"1999","Pumpkins"));
+		tracks.put(12, new Track(12,"Loosing","REM"));
+		tracks.put(13, new Track(13,"1999","Pumpkins"));
+	}
+
+	public List<Track> getAllTracks() {
+		
+		List<Track> trackList = new ArrayList<Track>();
+		
+		for (Object value : tracks.values()) {
+		    Track track = (Track)value;
+		    trackList.add(track);
+		}
+		
+		return trackList;
+	}
+	
+	public Track getTrackById(Integer id) {
+		
+		Track track = tracks.get(id);
+
+        return track;
 	}
 
 	public Track getTrackByTitle(String name) {
@@ -31,6 +53,20 @@ public class TrackDto {
 		tracks.put(track.getId(), track);
 		
 		String result = "Track saved : " + track;
+		
+		return result;
+	}
+	
+	public void deleteTrackById(Integer id) {
+		
+		tracks.remove(id);
+	}
+	
+	public String updateTrack(Track track) {
+		
+		tracks.put(track.getId(), track);
+		
+		String result = "Track updated : " + track;
 		
 		return result;
 	}
